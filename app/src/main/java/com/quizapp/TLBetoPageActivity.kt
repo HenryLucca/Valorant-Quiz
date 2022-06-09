1943ec6ae55eed304a2c06961bc5cac314b55c43
Matheus <matheus.albuquerque@hotmail.com>
2022-06-09 18:01:25 -0300

    Finalizando pagina individual

--- app/src/main/java/com/quizapp/TLBetoPageActivity.kt
+++ app/src/main/java/com/quizapp/TLBetoPageActivity.kt
@@ -1,11 +1,17 @@
 package com.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import kotlinx.android.synthetic.main.activity_henry_lucca_page.*
 
 class TLBetoPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tlbeto_page)
+        btn_back.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
